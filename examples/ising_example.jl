211dae2b0d588a7b24c3cd84c097a97eb7a28244
Ashley Milsted <ashmilsted@gmail.com>
2017-07-25 21:33:24 -0400

    Add some fun stuff to the Ising example.

--- examples/ising_example.jl
+++ examples/ising_example.jl
@@ -10,11 +10,28 @@ N = 32
 
 M = rand_puMPState(Complex128, 2, D, N)
 
-minimize_energy_local!(M, ising_local_MPO(Complex128), 100, step=0.1)
+minimize_energy_local!(M, ising_local_MPO(Complex128), 1000, step=0.1)
 
 println("Computing excitations!")
 
-ens, ks, exs = excitations!(M, ising_PBC_MPO_split(Complex128), [-2,-1,0,1,2], [5,4,7,4,5])
+ks_tocompute = [-2,-1,0,1,2]
+num_states = [5,4,7,4,5]
+
+ens, ks, exs = excitations!(M, ising_PBC_MPO_split(Complex128), ks_tocompute, num_states)
+
+H1 = Hn_in_basis(M, ising_Hn_MPO_split(Complex128, 1, N), exs, ks_tocompute)
+H2 = Hn_in_basis(M, ising_Hn_MPO_split(Complex128, 2, N), exs, ks_tocompute)
+
+ind1 = indmin(real(ens))
+indT = indmax(abs(H2[:,ind1]))
+
+en1 = ens[ind1]
+enT = ens[indT]
+
+fac = 2.0 / (enT-en1)
+
+c = 2*abs2(H2[indT,ind1] * fac)
+@show real(c)
 
 using PyPlot
 
