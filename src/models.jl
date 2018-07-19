9b16ba059dc4ce655ea7d8e6bc622620ed723748
Ashley Milsted <ashmilsted@gmail.com>
2018-07-19 16:11:56 -0400

    Add O'Brien Fendley supersymmetric model. Also some more exports.

--- src/models.jl
+++ src/models.jl
@@ -227,6 +227,120 @@ end
 
 #------------------------------
 
+function OBF_local_MPO{T}(::Type{T}; lambda1::Float64=1.0, lambda2::Float64=1.0)::MPO_open{T}
+    X = [0.0 1.0; 1.0 0.0]
+    Z = [1.0 0.0; 0.0 -1.0]
+    e = eye(2)
+    
+    h1 = zeros(Float64, 2,2,1,3)
+    h2 = zeros(Float64, 2,2,3,2)
+    h3 = zeros(Float64, 2,2,2,1)
+    
+    h1[:,:,1,1] = lambda2*Z
+    h1[:,:,1,2] = lambda2*X - lambda1*e
+    h1[:,:,1,3] = -lambda1*e
+    
+    h2[:,:,1,1] = Z
+    h2[:,:,2,2] = Z
+    h2[:,:,3,1] = e
+    
+    h3[:,:,1,1] = X
+    h3[:,:,2,1] = Z
+    
+    h1 = permutedims(h1, (3,2,4,1)) #[m1,ket,m2,bra]
+    h2 = permutedims(h2, (3,2,4,1)) #[m1,ket,m2,bra]
+    h3 = permutedims(h3, (3,2,4,1)) #[m1,ket,m2,bra]
+    
+    h1 = convert(MPOTensor{T}, h1)
+    h2 = convert(MPOTensor{T}, h2)
+    h3 = convert(MPOTensor{T}, h3)
+    
+    MPOTensor{T}[h1, h2, h3]
+end
+
+function OBF_OBC_MPO{T}(::Type{T}; lambda1::Float64=1.0, lambda2::Float64=1.0)::MPO_open_uniform{T}
+    X = [0.0 1.0; 1.0 0.0]
+    Z = [1.0 0.0; 0.0 -1.0]
+    e = eye(2)
+    
+    hM = zeros(Float64, 2,2,6,6)
+    
+    hM[:,:,1,1] = e
+    hM[:,:,2,1] = X
+    hM[:,:,3,1] = Z
+    hM[:,:,4,2] = Z
+    hM[:,:,5,3] = Z
+    hM[:,:,6,1] = -lambda1*X
+    hM[:,:,6,3] = -lambda1*Z
+    hM[:,:,6,4] = lambda2*Z
+    hM[:,:,6,5] = lambda2*X
+    hM[:,:,6,6] = e
+    
+    hL = zeros(Float64, 2,2,1,6)
+    hR = zeros(Float64, 2,2,6,1)
+    
+    hL[:,:,1,:] = hM[:,:,6,:]
+    hR[:,:,:,1] = hM[:,:,:,1]
+    
+    hM = permutedims(hM, (3,2,4,1)) #[m1,ket,m2,bra]
+    hL = permutedims(hL, (3,2,4,1)) #[m1,ket,m2,bra]
+    hR = permutedims(hR, (3,2,4,1)) #[m1,ket,m2,bra]
+    
+    hM = convert(MPOTensor{T}, hM)
+    hL = convert(MPOTensor{T}, hL)
+    hR = convert(MPOTensor{T}, hR)
+    
+    (hL, hM, hR)
+end
+
+function OBF_PBC_MPO_split{T}(::Type{T}; lambda1::Float64=1.0, lambda2::Float64=1.0)::MPO_PBC_uniform_split{T}
+    #Turn off hz for boundary terms, since they're already in the OBC part.
+    hL, hM, hR = OBF_OBC_MPO(T, lambda1=lambda1, lambda2=lambda2)
+    
+    #Pick out only the needed terms from the OBC Hamiltonian. Reduces the max. bond dimension to 3.
+    hL = hL[:,:,4:6,:]
+    hM1 = hM[4:6,:,2:5,:]
+    hM2 = hM[2:5,:,1:3,:]
+    hR = hR[1:3,:,:,:]
+    
+    h_B = MPOTensor{T}[hL, hM1, hM2, hR]
+    
+    (OBF_OBC_MPO(T, lambda1=lambda1, lambda2=lambda2), h_B)
+end
+
+function OBF_Hn_MPO_split{T}(::Type{T}, n::Int, N::Int; lambda1::Float64=1.0, lambda2::Float64=1.0)
+    (hL, hM, hR), (hb1, hb2, hb3, hb4) = OBF_PBC_MPO_split(T, lambda1=lambda1, lambda2=lambda2)
+    
+    Z = [1.0 0.0; 0.0 -1.0]
+
+    hL[1,:,1,:] *= cis(n*2π/N)
+    hL[1,:,3,:] *= cis(n*1.5*2π/N)
+    hL[1,:,4:5,:] *= cis(n*2*2π/N)
+
+    get_hM = (j::Int)->begin
+        hM_j = copy(hM)
+        hM_j[6,:,1,:] *= cis(n*j*2π/N)
+        hM_j[6,:,3,:] *= cis(n*(j+0.5)*2π/N)
+        hM_j[6,:,4:5,:] *= cis(n*(j+1)*2π/N)
+        hM_j
+    end
+
+    Hn_OBC = MPOTensor{T}[hL, (get_hM(n) for n in 2:N-1)..., hR]
+
+    #Boundary tensor for site N
+    hb2[3,:,2,:] *= cis(n*0.5*2π/N)
+    hb2[3,:,3:4,:] *= cis(n*1*2π/N)
+
+    #Boundary tensor for site N-1
+    #hb1[1,:,1:2,:] *= cis(n*N*2π/N) (does nothing)
+    
+    Hn_b = MPOTensor{T}[hb1, hb2, hb3, hb4]
+
+    n, (Hn_OBC, Hn_b)
+end
+
+#------------------------------
+
 function weylops(p::Int)
     om = cis(2π / p)
     U = diagm(Complex128[om^j for j in 0:p-1])
--- src/puMPS.jl
+++ src/puMPS.jl
@@ -14,15 +14,19 @@ export puMPState, rand_puMPState, mps_tensor, num_sites, set_mps_tensor!,
        canonicalize_left!,
        minimize_energy_local!, vumps_opt!
 
+export MPO_PBC_uniform, MPO_open_uniform, MPO_PBC_uniform_split, MPO_PBC_split
+
 include("states.jl")
 
-export excitations!, excitations, tangent_space_metric_and_MPO, tangent_space_MPO,
+export puMPSTvec, tvec_tensor, momentum, mps_tensors, excitations!, excitations, 
+       tangent_space_metric_and_MPO, tangent_space_MPO,
        Hn_in_basis, overlap, fidelity
 
 include("tangentspace.jl")
 
 export ising_local_MPO, ising_PBC_MPO, ising_PBC_MPO_split, ising_OBC_MPO, ising_Hn_MPO_split,
        ANNNI_local_MPO, ANNNI_OBC_MPO, ANNNI_PBC_MPO_split, ANNNI_Hn_MPO_split,
+       OBF_local_MPO, OBF_OBC_MPO, OBF_PBC_MPO_split, OBF_Hn_MPO_split,
        potts3_local_MPO, potts3_OBC_MPO, potts3_PBC_MPO_split, potts3_Hn_MPO_split
 
 include("models.jl")
