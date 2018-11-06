ef52ed0af85cf609b482f742498ff20a88a6acfe
Ashley Milsted <ashmilsted@gmail.com>
2018-11-06 16:36:36 -0500

    Fix depwarns in examples.

--- examples/ising_example.jl
+++ examples/ising_example.jl
@@ -1,29 +1,24 @@
-
-include("../src/MPS.jl")
-
-include("../src/puMPS.jl")
-
-using MPS, puMPS
+using puMPS
 
 D = 8
 N = 32
 
-M = rand_puMPState(Complex128, 2, D, N)
+M = rand_puMPState(ComplexF64, 2, D, N)
 
-minimize_energy_local!(M, ising_local_MPO(Complex128), 1000, step=0.1)
+minimize_energy_local!(M, ising_local_MPO(ComplexF64), 1000, step=0.1)
 
 println("Computing excitations!")
 
 ks_tocompute = [-2,-1,0,1,2]
 num_states = [5,4,7,4,5]
 
-ens, ks, exs = excitations!(M, ising_PBC_MPO_split(Complex128), ks_tocompute, num_states)
+ens, ks, exs = excitations!(M, ising_PBC_MPO_split(ComplexF64), ks_tocompute, num_states)
 
-H1 = Hn_in_basis(M, ising_Hn_MPO_split(Complex128, 1, N), exs, ks_tocompute)
-H2 = Hn_in_basis(M, ising_Hn_MPO_split(Complex128, 2, N), exs, ks_tocompute)
+H1 = Hn_in_basis(M, ising_Hn_MPO_split(ComplexF64, 1, N), exs, ks_tocompute)
+H2 = Hn_in_basis(M, ising_Hn_MPO_split(ComplexF64, 2, N), exs, ks_tocompute)
 
-ind1 = indmin(real.(ens))
-indT = indmax(abs.(H2[:,ind1]))
+ind1 = argmin(real.(ens))
+indT = argmax(abs.(H2[:,ind1]))
 
 en1 = ens[ind1]
 enT = ens[indT]
