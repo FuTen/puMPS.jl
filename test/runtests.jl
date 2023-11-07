aa19c19cbe3d705c597d1530a6b52dfe8e5d7c7e
Jan Schneider <jan.schneider@iff.csic.es>
2023-11-07 11:55:57 +0100

    correct test template

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-using puMPS
-using Test
-
-@testset "puMPS.jl" begin
-    # Write your tests here.
-end
-=======
 using LinearAlgebra
 using TensorOperations
 
@@ -13,5 +5,6 @@ using Test
 using puMPS
 using puMPS.MPS
 
-include("mps.jl")
->>>>>>> 103cb1a03a863c40240f0c6a192ec2cd7cc954e7
+@testset "puMPS.jl" begin
+    include("mps.jl")
+end
